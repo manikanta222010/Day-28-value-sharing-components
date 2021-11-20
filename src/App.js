1b5200e30626ded340327af8398ca87c0c812a52
Mani <manikanta222010gmail.com>
2021-11-20 23:17:01 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import { PieGraph } from './PieGraph';
+import { LineGraph } from './LineGraph';
+import { ColorBars } from './ColorBars';
+import { Colors } from './Colors';
+import { Aggregate } from './Aggregate';
+import { Header } from './Header';
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header component was imported here */}
+      <Header />
+      <Container maxWidth="lg">
+        {/* Aggregate details was displayed on Dashboard using this component */}
+        <Aggregate />
+
+        <Grid container spacing={2} sx={{ alignItems: 'flex-end' }}>
+          <Grid item xs={12} md={8}>
+            {/* Line Graph was imported here */}
+            <LineGraph />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            {/* Pie Graph was imported here */}
+            <PieGraph />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} >
+          <Grid item xs={12} md={6} >
+            {/* ColorBars were imported here */}
+            <ColorBars />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            {/* Colors were imported here */}
+            <Colors />
+          </Grid>
+        </Grid>
+
+      </Container>
     </div>
   );
 }
 
-export default App;
+
